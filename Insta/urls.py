2142db7ff020e3fc10c941622464b422b31429d8
forestGr <cctnsw@gmail.com:q!:wq>
2020-08-07 22:23:41 -0700

    display static page

--- Insta/urls.py
+++ Insta/urls.py
@@ -1,4 +1,4 @@
-"""JZDEV URL Configuration
+"""InstaJZ URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from Insta.views import HelloWorld
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', HelloWorld.as_view(), name='helloworld'),
 ]
